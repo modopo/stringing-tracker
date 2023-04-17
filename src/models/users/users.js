49bc720b408adb92a23c07d6c6515eefb6b6d9ec
Jonathan Tsai <modopo@gmail.com>
2023-04-16 17:17:54 -0700

    beforeCreate and auth

--- src/models/users/users.js
+++ src/models/users/users.js
@@ -31,4 +31,31 @@ const userModel = (sequelize, DataTypes) => {
       }
     }
   })
+
+  model.beforeCreate(async (user) => {
+    let hashedPassword = await bycrpt.hash(user.password, 10);
+    user.password = hashedPassword;
+  })
+
+  model.authBasic = async function (username, password) {
+    const user = await this.findOne({ where: { username: username }})
+    const valid = await bcrypt.compare(password, user.password);
+    if (valid) { return user; }
+    throw new Error("Invalid credentials");
+  }
+
+  model.authToken = async function (token) {
+    try {
+      const parsedToken = jwt.verify(token, SECRET);
+      const user = this.findOne({ where: { username: parsedToken.username } });
+      if (user) { return user; };
+      throw new Error("User not found");
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  return model;
 }
+
+module.exports = model;
